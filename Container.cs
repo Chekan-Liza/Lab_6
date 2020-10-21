d02d5f8a950a1b81f93773fecb93714c7ab4ab55
Chekan-Liza <liza.chekan@gmail.com>
2020-10-21 20:10:03 +0300

    container

---
+++ Container.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OAP_Laba_6 {
+  class Bookkeeping {
+
+    public List<Document> data = new List<Document>();
+ 
+    public static int counter = 0;
+
+    public void Output( ) {
+      if ( counter == 0 ) {
+        Console.WriteLine( "В бухгалтерии нет сведений!" );
+      }
+      else {
+        foreach ( object i in data ) {
+          Console.WriteLine( $"Объект {i}" );
+        }
+      }
+    }
+
+    public void Remove(Document A ) {
+      data.Remove( A );
+    }
+
+    public void ClearLibrary( ) {
+      if ( counter == 0 ) {
+        Console.WriteLine( "Бухгалтерия пуста" );
+      }
+      else {
+        counter = 0;
+        data.Clear();
+        Console.WriteLine( "Бухгалтерия очищена" );
+
+      }
+    }
+
+    public void Push(Document N ) {
+      data.Add( N );
+      counter++;
+    }
+  }
+}
