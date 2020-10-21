8a3d86456f2b7134b2d28f88958f864901d2afa9
Chekan-Liza <liza.chekan@gmail.com>
2020-10-21 20:00:11 +0300

    Partial class Invoice

---
+++ Invoice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OAP_Laba_6 {
+  public partial class Invoice
+    {
+    public bool DocConfirmed( ) {
+      this.IsConfirmed = true;
+      return true;
+    }
+  }
+}
